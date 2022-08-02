2bbc1c9a09bf0397c39cda082ba6aef906efc48b
Youngsung <sysgigigi@gmail.com>
2022-08-02 15:25:13 +0900

    TSV2-112 : Feat - set dashboard as a default page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 /*
 ************
@@ -11,6 +13,12 @@ AUG 8, 2022
 */
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
+
   return (
     <div>
       <Head>
