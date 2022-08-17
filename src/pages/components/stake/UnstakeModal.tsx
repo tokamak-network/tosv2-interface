c9c13bb1ae487834daa89ba242302e49c194ba63
Lakmi <lakmi94@gmail.com>
2022-08-17 15:25:00 +0900

    Update UnstakeModal.tsx

--- src/pages/components/stake/UnstakeModal.tsx
+++ src/pages/components/stake/UnstakeModal.tsx
@@ -16,7 +16,7 @@ import {
   SliderFilledTrack,
   SliderThumb,
   SliderMark,
-  Tooltip
+  Tooltip,
 } from "@chakra-ui/react";
 // import { CloseIcon } from "@chakra-ui/icons";
 import { useRecoilValue } from "recoil";
@@ -27,6 +27,8 @@ import CLOSE_ICON from "assets/icons/close-modal.svg";
 import CustomCheckBox from "common/input/CustomCheckBox";
 import SubmitButton from "common/button/SubmitButton";
 import { useState } from "react";
+import question from "assets/icons/question.svg";
+import { TextInput, BalanceInput } from "common/input/TextInput";
 
 function StakeGraph() {
   const labelStyles = {
@@ -36,17 +38,16 @@ function StakeGraph() {
   };
   const [sliderValue, setSliderValue] = useState(36);
   const { colorMode } = useColorMode();
-  const [showTooltip, setShowTooltip] = useState(false)
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
     <Flex w={"100%"} h={"70px"} pos="relative">
       {/* diagonal line */}
-      
+
       <Slider
         aria-label="slider-ex-1"
         defaultValue={0}
         min={0}
         max={156}
-       
         onChange={(val: any) => setSliderValue(val)}
         h={"10px"}
         alignSelf={"end"}
@@ -57,44 +58,90 @@ function StakeGraph() {
           7d
         </SliderMark>
         <SliderMark value={8} {...labelStyles}>
-       1m
+          1m
         </SliderMark>
         <SliderMark value={24} {...labelStyles}>
-         6m
+          6m
         </SliderMark>
         <SliderMark value={52} {...labelStyles}>
-       1y
+          1y
         </SliderMark>
         <SliderMark value={104} {...labelStyles}>
-        2y
+          2y
         </SliderMark>
         <SliderMark value={156} {...labelStyles}>
-        3y
+          3y
         </SliderMark>
-        <SliderTrack bg={colorMode ==='light'? '#e7edf3':'#353d48'}>
-          <SliderFilledTrack bg={'#2775ff'}/>
-        </SliderTrack >
+        <SliderTrack bg={colorMode === "light" ? "#e7edf3" : "#353d48"}>
+          <SliderFilledTrack bg={"#2775ff"} />
+        </SliderTrack>
         <Tooltip
-        color={colorMode === 'light'? '#07070c':'#f1f1f1'}
-        placement='top'
-        bg={'transparent'}
-        w={'50px'}
-        display='flex'
-        alignItems='center'
-        justifyContent={'center'}
-       textAlign='center'
-        fontSize={'15px'}
-      fontWeight={600}
-        isOpen={showTooltip}
-        label={`${sliderValue} sTOS`}
-
-      >
-       
-         <SliderThumb />
-      </Tooltip>
-       
+          color={colorMode === "light" ? "#07070c" : "#f1f1f1"}
+          placement="top"
+          bg={"transparent"}
+          w={"50px"}
+          display="flex"
+          alignItems="center"
+          justifyContent={"center"}
+          textAlign="center"
+          fontSize={"15px"}
+          fontWeight={600}
+          isOpen={showTooltip}
+          label={`${sliderValue} sTOS`}
+        >
+          <SliderThumb />
+        </Tooltip>
       </Slider>
     </Flex>
+  );
+}
+
+function Tile(props: { title: string; content: string; symbol?: string }) {
+  const { title, content, symbol } = props;
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      display={"flex"}
+      flexDir={"column"}
+      w={"152px"}
+      alignItems={"center"}
+      mb={"15px"}
+    >
+      <Flex alignItems={"center"}>
+        <Text
+          color={colorMode === "dark" ? "gray.100" : "gray.1000"}
+          h={"17px"}
+          mb={"3px"}
+          fontWeight={600}
+          fontSize={12}
+          textAlign="center"
+          mr={"6px"}
+        >
+          {title}
+        </Text>
+        <Tooltip label="" placement="bottom">
+          <Image src={question} alt={""} height={"16px"} width={"16px"} />
+        </Tooltip>
+      </Flex>
+
+      <Flex fontWeight={"bold"} h={"33px"}>
+        <Text
+          color={colorMode === "dark" ? "white.100" : "gray.800"}
+          fontSize={24}
+          mr={2}
+        >
+          {content}
+        </Text>
+        <Text
+          color={colorMode === "dark" ? "white.200" : "gray.800"}
+          fontSize={14}
+          pt={"5px"}
+          lineHeight={"33px"}
+        >
+          {symbol ? symbol : ""}
+        </Text>
+      </Flex>
+    </Box>
   );
 }
 
@@ -104,8 +151,13 @@ function BottomContent(props: { title: string; content: string }) {
   return (
     <Flex>
       <Flex w={"100%"} justifyContent={"space-between"} fontSize={14}>
-        <Text color={colorMode === 'dark'? "gray.100":'gray.1000'}>{title}</Text>
-        <Text color={colorMode === 'dark'?"white.200":'gray.800'} fontWeight={600}>
+        <Text color={colorMode === "dark" ? "gray.100" : "gray.1000"}>
+          {title}
+        </Text>
+        <Text
+          color={colorMode === "dark" ? "white.200" : "gray.800"}
+          fontWeight={600}
+        >
           {content}
         </Text>
       </Flex>
@@ -149,7 +201,11 @@ function UnstakeModal() {
             <Flex flexDir={"column"} pos={"relative"}>
               {/* Title Area*/}
               <Flex w={"100%"} justifyContent={"center"} mb={"33px"}>
-                <Text color={colorMode === "light" ? 'gray.800':"white.200"} fontSize={20} fontWeight={600}>
+                <Text
+                  color={colorMode === "light" ? "gray.800" : "white.200"}
+                  fontSize={20}
+                  fontWeight={600}
+                >
                   Unstake
                 </Text>
                 <Flex
@@ -163,9 +219,34 @@ function UnstakeModal() {
               </Flex>
             </Flex>
             {/* Content Area*/}
-            <Flex w={"100%"} flexDir={"column"} mb={"30px"}>
+            <Flex w={"100%"}  px={"120px"}  flexDir={"column"} mb={"30px"}>
               {/* Content Bottom */}
-              <Flex flexDir={"column"} rowGap={"9px"} px={"50px"}>
+            
+              <Flex w={"100%"} justifyContent={"space-between"} mb={"9px"}>
+                  <Tile title={"Next Rebase"} content={"05:50:20"} />
+
+                  <Tile title={"LTOS Index"} content={"100"} symbol={"TOS"} />
+                </Flex>
+                <Flex mb={"9px"}>
+                  <BalanceInput
+                    w={"100%"}
+                    h={45}
+                    atomKey={"stake_stake_modal_balance"}
+                  ></BalanceInput>
+                </Flex>
+                <Flex
+                  fontSize={12}
+                  color={colorMode === "dark" ? "#8b8b93" : "gray.1000"}
+                  h={"17px"}
+                  justifyContent={"space-between"}
+                  mb={"12px"}
+                >
+                  <Text>Your Balance</Text>
+                  <Text>1,000 WTON</Text>
+                </Flex>
+                 
+            </Flex>
+            <Flex flexDir={"column"} rowGap={"9px"} px={"50px"} mb={'30px'}>
                 {contentList.map((content, index) => {
                   return (
                     <BottomContent
@@ -176,14 +257,13 @@ function UnstakeModal() {
                   );
                 })}
               </Flex>
-            </Flex>
             <Flex flexDir={"column"} alignItems={"center"} rowGap={"15px"}>
               <SubmitButton w={460} h={42} name="Unstake"></SubmitButton>
-              <SubmitButton
+              {/* <SubmitButton
                 w={460}
                 h={42}
                 name="Stake to get sTOS"
-              ></SubmitButton>
+              ></SubmitButton> */}
             </Flex>
           </Flex>
         </ModalBody>
