6606cf136482748ed6721c3c73bba0de012a4b5e
Youngsung <sysgigigi@gmail.com>
2022-09-21 13:40:33 +0900

    TSV-153 : Stake - TipCard

--- src/pages/stake.tsx
+++ src/pages/stake.tsx
@@ -6,6 +6,7 @@ import UnstakeModal from "pages/components/stake/UnstakeModal";
 import UpdateModal from "pages/components/stake/UpdateModal";
 import UpdateModalAfterEndTime from "pages/components/stake/UpdateModalAfterEndTime";
 import TopCardContainer from "./components/common/card/TopCardContainer";
+import TipCard from "./components/common/tip/TipCard";
 import MultiUnstakeModal from "./components/stake/MultiUnstakeModal";
 
 const Stake = () => {
@@ -17,6 +18,12 @@ const Stake = () => {
       bg={colorMode === "light" ? "#fafbfc" : "black.100"}
     >
       <PageLayout></PageLayout>
+      <TipCard
+        msg={[
+          "Tip: LTOS can be unstaked for TOS. # of TOS you get is based on the LTOS index, where # of TOS = # of LTOS x LTOS index.",
+          "Every 8 hours LTOS index is increased and LTOS can be unstaked for more TOS.",
+        ]}
+      ></TipCard>
       <TopCardContainer pageKey={"Stake_screen"}></TopCardContainer>
       <StakeCardContainer></StakeCardContainer>
       <StakeModal></StakeModal>
