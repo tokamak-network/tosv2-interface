8ffb17e7e8af045bf107592460ba4641b81534a5
Youngsung <sysgigigi@gmail.com>
2022-10-06 10:59:26 +0900

    Fix ts error

--- src/types/modal.ts
+++ src/types/modal.ts
@@ -4,6 +4,7 @@ export type StakeModalType =
   | "stake_stake_modal"
   | "stake_unstake_modal"
   | "stake_update_modal"
-  | "stake_multiUnstake_modal";
+  | "stake_multiUnstake_modal"
+  | "stake_updateAfterEndTime_modal";
 
 export type ModalType = BondModalType | StakeModalType;
