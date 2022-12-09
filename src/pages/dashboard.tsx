0b25067ee793da5ecdcc8442a39b517886579c3c
Youngsung <sysgigigi@gmail.com>
2022-12-09 10:25:29 +0900

    TSV2-210 : Fix - Dashboard _ setInitialSelectedFilter

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,21 @@
 import { Flex, useTheme, useColorMode } from "@chakra-ui/react";
+import { filterState } from "atom/dashboard";
 import GraphContainer from "pages/components/dashboard/GraphContainer";
 import SmallCardContainer from "pages/components/dashboard/SmallCardContainer";
 import PageLayout from "pages/components/layout/PageLayout";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 
 const DashBoard = () => {
   const theme = useTheme();
   const { colorMode } = useColorMode();
+
+  const [selectedFilter, setSelectedFilter] = useRecoilState(filterState);
+
+  useEffect(() => {
+    setSelectedFilter("1 Week");
+  }, []);
+
   return (
     <Flex
       {...theme.PAGE_LAYOUT_STYLE.layoutTheme(colorMode)}
