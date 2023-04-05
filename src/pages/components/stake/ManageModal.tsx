8a5383fc6470064fd01d24b7499e7a6416e16809
SonYoungsung <sysgigigi@gmail.com>
2023-04-05 15:00:48 +0900

    Fix : ts-error

--- src/pages/components/stake/ManageModal.tsx
+++ src/pages/components/stake/ManageModal.tsx
@@ -73,8 +73,8 @@ import ManageModal_BottomContent from "./modal/ManageModal_BottomContent";
 import useMediaView from "hooks/useMediaView";
 import StakeModal_Input from "@/stakeComponents/modal/components/StakeModal_Input";
 import StakeModal_Period from "@/stakeComponents/modal/components/StakeModal_Period";
-import { Manage_Period } from "@/stakeComponents/modal/components/Manage_Period";
-import { UserGuide } from "../common/guide/UserGuide";
+import Manage_Period from "@/stakeComponents/modal/components/Manage_Period";
+import UserGuide from "../common/guide/UserGuide";
 
 function ManageModal() {
   const theme = useTheme();
